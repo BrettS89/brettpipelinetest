9a59d0a622b7c3e7991a52ce90645056763853f5
brettsodie <blsodie@gmail.com>
2021-06-15 18:21:55 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('in');
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+});
